8649648209b9be991d21d8229fe15db046748a26
P.O Causs <112474708+gpocil@users.noreply.github.com>
2024-07-10 11:03:29 +0200

    Delete src directory

--- src/routes/BusinessRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as ImagesController from '../controllers/BusinessController'
-const router = Router();
-
-// router.post('/user', GoogleController.fetchImageUrlsFromUserAccount);
-router.post('/images', ImagesController.getPhotos);
-
-
-export default router;
