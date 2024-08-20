0de840ca168e17900f0f96327f744a9642570581
Pierre-Olivier Caussarieu <pocaussarieu@gmail.com>
2024-08-20 09:26:02 -0700

    ssl

--- front-end/src/util/config.ts
+++ front-end/src/util/config.ts
@@ -1,6 +1,6 @@
 const config = {
     apiBaseUrlDev: 'http://localhost:3000/api',
-    apiBaseUrlProd: 'http://37.187.35.37:3000/api',
+    apiBaseUrlProd: 'https://37.187.35.37:3000/api',
     dev: false
 };
 
