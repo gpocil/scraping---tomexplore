7d02f043a4fac2e144aae621e69e58c430b53294
Pierre-Olivier Caussarieu <pocaussarieu@gmail.com>
2024-08-20 09:27:45 -0700

    ssl

--- front-end/src/util/config.ts
+++ front-end/src/util/config.ts
@@ -1,6 +1,6 @@
 const config = {
     apiBaseUrlDev: 'http://localhost:3000/api',
-    apiBaseUrlProd: 'https://37.187.35.37:3000/api',
+    apiBaseUrlProd: 'https://monblogdevoyage.com/api',
     dev: false
 };
 
