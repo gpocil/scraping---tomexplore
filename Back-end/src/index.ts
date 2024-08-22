47628b2edeb4e1885850f5888802aded541a93f0
Pierre-Olivier Caussarieu <pocaussarieu@gmail.com>
2024-08-22 06:15:02 -0700

    ta race

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import cors, { CorsOptions } from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
+import https from 'https';
+import fs from 'fs';
 import scrapingMainRoutes from './routes/scraping/ScrapingMainRoutes';
 import InstagramRoutes from './routes/scraping/InstagramRoutes';
 import authRoutes from './routes/security/LoginRoutes';
@@ -23,26 +25,77 @@ import './models';
 const app = express();
 const port = 3000;
 
+// Load SSL certificate and key
+const sslOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/monblogdevoyage.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/monblogdevoyage.com/fullchain.pem')
+};
 
-app.use(cors()); // Applique CORS à toutes les origines par défaut
+// const allowedOrigins = [
+//     'https://localhost:3001',
+//     'https://localhost:3001/login',
+//     'https://localhost',
+//     'https://37.187.35.37:3001',
+//     'https://37.187.35.37:3001/login',
+//     'https://37.187.35.37',
+//     'https://37.187.35.37:3000/api-docs',
+//     'https://37.187.35.37:3000',
+//     'https://localhost:3000/api-docs',
+//     'https://localhost:3000',
+//     'https://monblogdevoyage.com',
+//     'https://monblogdevoyage.com/api-docs',
+//     'http://localhost:3001',
+//     'http://localhost:3001/login',
+//     'http://localhost',
+//     'http://37.187.35.37:3001',
+//     'http://37.187.35.37:3001/login',
+//     'http://37.187.35.37',
+//     'http://37.187.35.37:3000/api-docs',
+//     'http://37.187.35.37:3000',
+//     'http://localhost:3000/api-docs',
+//     'http://localhost:3000',
+//     'http://monblogdevoyage.com',
+//     'http://monblogdevoyage.com/api-docs',
+//     '149.202.90.176',
+//     'https://tomexplore.com',
+//     'https://www.tomexplore.com'
+// ];
+// console.log('Lancement CORS'); 
+
+// const corsOptions: CorsOptions = {
+//     origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+//         console.log('Origin:', origin); // Ajoutez cette ligne pour le débogage
+//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             console.log('Blocked by CORS:', origin); // Et cette ligne pour le débogage
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// };
+
+
+
+app.use(cors());
 console.log('CORS middleware is now applied to all origins.');
 
 app.use(bodyParser.json());
-
-// Configuration Swagger
 const specs = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-// Routes API
 app.use('/api/auth', authRoutes);
 app.use('/api/front', frontRoutes);
 const imagesPath = path.join(__dirname, 'temp');
-console.log('Image path: ' + imagesPath);
+console.log('imagepath : ' + imagesPath);
 app.use('/images', express.static(imagesPath));
 app.use('/api', QueueRoutes);
 
-// Routes nécessitant l'authentification
-// app.use(jwtMiddleware);
+//------------------------Auth required---------------------------------
+
+app.use(jwtMiddleware);
 app.use('/api/texplore', scrapingMainRoutes);
 app.use('/api/texplore', texploreRoutes);
 app.use('/api/unsplash', unsplashRoutes);
@@ -52,26 +105,24 @@ app.use('/api', wikimediaRoutes);
 app.use('/api', fileRoutes);
 app.use('/api', googleRoutes);
 
-// Servir les fichiers statiques de l'application React
-app.use(express.static(path.join(__dirname, '../../front-end/build')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'front-end/build')));
 
-// Catch-all pour toutes les autres routes
+// The "catchall" handler: for any requesthat doesn't match one above, send back index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'front-end/build', 'index.html'));
 });
 
-// Connexion à la base de données et démarrage du serveur
 sequelize.authenticate()
     .then(() => {
-        console.log('Connection to the database has been established successfully.');
+        console.log('Connection has been established successfully.');
 
         sequelize.sync()
             .then(() => {
                 console.log('Database & tables created!');
 
-                // Démarrage du serveur en HTTP
-                app.listen(port, () => {
-                    console.log(`Server running at http://localhost:${port}/api-docs`);
+                https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+                    console.log(`Server running at https://localhost:${port}/api-docs`);
                 });
             })
             .catch(err => {
